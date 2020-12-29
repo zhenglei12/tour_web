c1282f1618bcc89cbe4ffa30cac2e9d412f86f94
靖成功 <jingchenggong@shall-buy.com>
2020-12-29 15:30:02 +0800

    1

--- src/directives/acl.js
+++ src/directives/acl.js
@@ -1,13 +1,9 @@
 import acl from '../libs/acl';
 
-const CLS = 'acl-hide';
-
 export default {
     inserted: function (el, binding) {
-        if (!binding.value || acl.verify(binding.value, binding.arg)) {
-            el.classList.remove(CLS);
-        } else {
-            el.classList.add(CLS);
+        if (binding.value && !acl.verify(binding.value, binding.arg)) {
+            el.parentNode.removeChild(el);
         }
     },
 }
