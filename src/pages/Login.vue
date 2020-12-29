9cb68815e6c5c4fe9a5c7bd81d75f8d333c45682
靖成功 <jingchenggong@shall-buy.com>
2020-12-29 18:10:54 +0800

    登录页

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,31 +1,33 @@
 <template>
-  <div class="login-card">
-    <a-form-model ref="form" class="login-form" :model="form" :rules="rules">
-      <a-form-model-item prop="username">
-        <a-input v-model="form.username" placeholder="用户名" allow-clear>
-          <a-icon slot="prefix" type="user" class="login-form-icon" />
-        </a-input>
-      </a-form-model-item>
-      <a-form-model-item prop="password">
-        <a-input
-          type="password"
-          v-model="form.password"
-          placeholder="密码"
-          allow-clear
+  <div class="wraper">
+    <div class="login-card">
+      <a-form-model ref="form" class="login-form" :model="form" :rules="rules">
+        <a-form-model-item prop="username">
+          <a-input v-model="form.username" placeholder="用户名" allow-clear>
+            <a-icon slot="prefix" type="user" class="login-form-icon" />
+          </a-input>
+        </a-form-model-item>
+        <a-form-model-item prop="password">
+          <a-input
+            type="password"
+            v-model="form.password"
+            placeholder="密码"
+            allow-clear
+          >
+            <a-icon slot="prefix" type="lock" class="login-form-icon" />
+          </a-input>
+        </a-form-model-item>
+        <a-button
+          class="login-submit"
+          size="large"
+          type="primary"
+          block
+          :loading="loading"
+          @click="login"
+          >登录</a-button
         >
-          <a-icon slot="prefix" type="lock" class="login-form-icon" />
-        </a-input>
-      </a-form-model-item>
-      <a-button
-        class="login-submit"
-        size="large"
-        type="primary"
-        block
-        :loading="loading"
-        @click="login"
-        >登录</a-button
-      >
-    </a-form-model>
+      </a-form-model>
+    </div>
   </div>
 </template>
 
@@ -76,19 +78,27 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.wraper {
+  height: 100%;
+  background-image: url("~@/assets/login.jpg");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+}
 .login {
   &-card {
     position: absolute;
-    width: 100%;
-    top: 35%;
-    left: 0;
+    width: 400px;
+    box-sizing: content-box;
+    top: 50%;
+    right: 150px;
     transform: translateY(-50%);
-    display: flex;
-    justify-content: center;
+    background: #ffffff;
+    padding: 40px 20px;
+    border-radius: 4px;
   }
 
   &-form {
-    width: 400px;
+    width: 100%;
 
     &-icon {
       color: rgba(0, 0, 0, 0.25);
